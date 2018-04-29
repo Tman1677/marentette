128873573601e801572c14f64e68c8ae339f29f0
Unknown <tristankreindler@gmail.com>
2018-04-29 14:22:30 -0400

    Basic index page

--- packets/urls.py
+++ packets/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
     path('chapter/<int:pk>', views.ChapterView.as_view(), name='chapter-view'),
+    path('chapter/', RedirectView.as_view(url='/')),
 ]
--- packets/views.py
+++ packets/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Video, ChapterPacket, OtherPacket
 # Create your views here.
-def index(request):
-    return render(
-        request,
-        'packets.html',
-    )
+
+class Index(generic.ListView):
+    model = ChapterPacket
+    context_object_name = 'chapter_list'
+    template_name = 'index.html'
 
 class ChapterView(generic.ListView):
     model = ChapterPacket
     context_object_name = 'chapter_list'
-    template = 'chapterpacket_list.html' #note I can't seem to change this
+    template_name = 'chapter_view.html' #note I can't seem to change this
     def get_context_data(self, *args, **kwargs):
         context = super(ChapterView, self).get_context_data(*args, **kwargs) #keep the base context normal
         context['video_list'] = Video.objects.filter(chapterPacket__number=self.kwargs['pk']) #add in our second video context
