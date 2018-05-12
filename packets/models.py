1f0c6071038ab484207c77b2e50471a815c2d1a0
Unknown <tristankreindler@gmail.com>
2018-05-11 23:54:58 -0400

    Change primary key

--- packets/models.py
+++ packets/models.py
@@ -7,7 +7,7 @@ class OtherPacket(models.Model):
     def __str__(self):
         return self.name
 class ChapterPacket(models.Model):
-    number = models.IntegerField(unique=True)
+    number = models.IntegerField(unique=True, primary_key = True)
     def name(self):
         return "Chapter " + str(self.number)
     def __str__(self):
