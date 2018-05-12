eb07e00b7330775962049c8f71092a28a3c3e676
Unknown <tristankreindler@gmail.com>
2018-05-11 22:02:19 -0400

    Change database

--- marentette/settings.py
+++ marentette/settings.py
@@ -76,13 +76,19 @@ WSGI_APPLICATION = 'marentette.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+SECRET_KEY = config('This isn't so secret since I'm deploying to github')
+DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('postgres://rpdalzzkypxgtg:eb6492bced1fdb980ee03dc3ca23b910e1c7f9003baa5a55e2a3a5429e9890e4@ec2-54-83-1-94.compute-1.amazonaws.com:5432/d7g3s8j2t22u04')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
